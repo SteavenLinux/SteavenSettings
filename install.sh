c82ae43d839fdc2f731cb07cfae704e4a20d1e29
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-06-22 17:08:10 +0300

    headset wasnt fixed, now its fixed , i hope

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 sudo cp -Rv var/* /var
 sudo cp -Rv usr/* /usr
 sudo cp -Rv etc/* /etc
+cp -Rv $PWD/home/user/.config/systemd/user/headset.service ~/.config/systemd/user/headset.service
 sudo systemctl enable battery_charge_threshold.service --now
 sudo systemctl enable headset.service --now
+systemctl --user enable headset.service --now
